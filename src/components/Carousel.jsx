3ccd02cd16475a665ac109483d5ed432fa765ace
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-03-18 11:46:01 -0500

    refactor: update Logo and Carousel components with new styles and layout adjustments

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,19 +7,19 @@ export default function Carousel() {
     if (typeof window !== "undefined") {
       new Swiper(".carrusel-container", {
         loop: true,
-        slidesPerView: 3, // Muestra 3 slides al mismo tiempo
+        slidesPerView: 3,
         autoplay: {
-          delay: 3000, // Desliza cada 3 segundos
-          disableOnInteraction: false, // No se detiene al interactuar
+          delay: 3000,
+          disableOnInteraction: false,
         },
       });
     }
   }, []);
 
   return (
-    <div className="grid grid-rows-1 grid-cols-4 h-full">
-      <div className="col-span-1 grid grid-cols-1 grid-rows-3 bg-blue-200 h-full p-3 py-4 gap-2">
-        <div className="row-span-1 bg-amber-50 grid grid-cols-2 grid-rows-1 rounded-xl">
+    <div className="grid grid-rows-1 grid-cols-4 h-full p-3">
+      <div className="text-white col-span-1 grid grid-cols-1 grid-rows-3 bg-[#233f80] h-full p-3 py-4 gap-2 rounded-l-xl">
+        <div className="row-span-1 bg-[#3a538c] grid grid-cols-2 grid-rows-1 rounded-xl">
           <div className="col-span-1 justify-center self-center text-center rounded-xl h-full w-auto">
             <img
               src="src/assets/dominican-flag.svg"
@@ -31,14 +31,14 @@ export default function Carousel() {
             DOMINICAN REPUBLIC
           </div>
         </div>
-        <div className="row-span-1 bg-amber-100 grid grid-cols-2 grid-rows-1 rounded-xl">
+        <div className="row-span-1 bg-[#3a538c] grid grid-cols-2 grid-rows-1 rounded-xl">
           <div className="self-center text-center flex flex-row justify-center items-center">
             <img
               src="src/assets/instagram-white.svg"
               alt="instagram"
               className="w-16 ml-6"
             />
-            <p className="text-white text-5xl font-thin opacity-70 self-center mb-3 ml-6">
+            <p className=" text-5xl font-thin opacity-70 self-center mb-3 ml-6">
               |
             </p>
           </div>
@@ -46,30 +46,61 @@ export default function Carousel() {
             <p>9K</p>
           </div>
         </div>
-        <div className="row-span-1 bg-amber-50 rounded-xl flex items-center justify-center">
-          <a href="http://instagram.com/tainobayport/">
+        <div className="row-span-1 bg-[#3a538c] rounded-xl flex items-center justify-center">
+          <a href="http://instagram.com/tainobayport/" target="_blank">
             <p className="text-3xl font-semibold">@TAINOBAYPORT</p>
           </a>
         </div>
       </div>
-      <div className="col-span-3 h-full">
-        <div className="swiper carrusel-container h-full bg-gray-200 p-4">
-          <div className="swiper-wrapper h-full">
-            <div className="swiper-slide h-full bg-red-500 flex items-center justify-center text-white text-xl font-bold">
-              Slide 1
+      <div className="col-span-3 w-full">
+        <div className="swiper carrusel-container h-full rounded-r-xl">
+          <div className="swiper-wrapper w-full">
+            <div className="swiper-slide h-full bg-[#233f80] flex items-center justify-center text-white text-xl font-bold py-4 px-8">
+              <div className="bg-[#091e57]  h-full flex flex-col justify-center items-center rounded-xl px-4 py4">
+                <img
+                  className="max-h-full w-full"
+                  src="src/assets/galery/galeryExample.png"
+                  alt="galeryexample"
+                />
+              </div>
             </div>
-            <div className="swiper-slide h-full bg-blue-500 flex items-center justify-center text-white text-xl font-bold">
-              Slide 2
+            <div className="swiper-slide h-full bg-[#233f80] flex items-center justify-center text-white text-xl font-bold py-4 px-8">
+              <div className="bg-[#091e57]  h-full flex flex-col justify-center items-center rounded-xl px-4 py4">
+                <img
+                  className="max-h-full w-full"
+                  src="src/assets/galery/galeryExample.png"
+                  alt="galeryexample"
+                />
+              </div>
             </div>
-            <div className="swiper-slide h-full bg-green-500 flex items-center justify-center text-white text-xl font-bold">
-              Slide 3
+            <div className="swiper-slide h-full bg-[#233f80] flex items-center justify-center text-white text-xl font-bold py-4 px-8">
+              <div className="bg-[#091e57]  h-full flex flex-col justify-center items-center rounded-xl px-4 py4">
+                <img
+                  className="max-h-full w-full"
+                  src="src/assets/galery/galeryExample.png"
+                  alt="galeryexample"
+                />
+              </div>
             </div>
-            <div className="swiper-slide h-full bg-yellow-500 flex items-center justify-center text-white text-xl font-bold">
-              Slide 4
+            <div className="swiper-slide h-full bg-[#233f80] flex items-center justify-center text-white text-xl font-bold py-4 px-8">
+              <div className="bg-[#091e57]  h-full flex flex-col justify-center items-center rounded-xl px-4 py4">
+                <img
+                  className="max-h-full w-full"
+                  src="src/assets/galery/galeryExample.png"
+                  alt="galeryexample"
+                />
+              </div>
             </div>
-            <div className="swiper-slide h-full bg-purple-500 flex items-center justify-center text-white text-xl font-bold">
-              Slide 5
+            <div className="swiper-slide h-full bg-[#233f80] flex items-center justify-center text-white text-xl font-bold py-4 px-8 ">
+              <div className="bg-[#091e57]  h-full flex flex-col justify-center items-center rounded-xl px-4 py4">
+                <img
+                  className="max-h-full w-full"
+                  src="src/assets/galery/galeryExample.png"
+                  alt="galeryexample"
+                />
+              </div>
             </div>
+            
           </div>
         </div>
       </div>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,8 @@
 export default function Logo() {
-  return <div className="flex items-center justify-center h-full rounded-lg p-4">
+  return <div className="flex items-center  justify-center h-full rounded-lg p-3">
+    <div className="flex items-center justify-center bg-[#233f80]  h-full w-full rounded-lg p-4">
     <img src="src/assets/logo-white.svg" alt="logo-taino" />
+
+    </div>
   </div>;
 }
