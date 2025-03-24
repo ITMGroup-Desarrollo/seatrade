4a1e515ae33d82c169c052ee4aa428a1e32c758e
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-03-24 15:39:34 -0500

    feat: add gallery image and enhance Carousel styles with new animations and layout adjustments

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,7 +19,7 @@ export default function Carousel() {
   return (
     <div className="grid grid-rows-1 grid-cols-12 h-full px-8 py-4">
       <div className="text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[#233f80]/50  h-full py-4 pl-4 gap-2 rounded-l-xl">
-        <div className="row-span-1 bg-custom-gradient grid grid-cols-2 grid-rows-1 rounded-xl">
+        <div className="row-span-1 bg-custom-gradient grid grid-cols-2 grid-rows-1 rounded-xl m-2 border-2 border-[#38568e]">
           <div className="col-span-1 justify-center self-center text-center rounded-xl h-full w-auto">
             <img
               src="src/assets/dominican-flag.svg"
@@ -27,76 +27,80 @@ export default function Carousel() {
               className="h-full w-auto"
             />
           </div>
-          <div className="col-span-1 justify-center self-center text-left text-lg font-semibold">
+          <div className="col-span-1 justify-center self-center text-left text-lg font-semibold pl-3">
             DOMINICAN REPUBLIC
           </div>
         </div>
-        <div className="row-span-1 flex items-center justify-center bg-custom-gradient h-full w-full rounded-xl p-4">
-          <img src="src/assets/logo-white.svg" alt="logo-taino" />
+        <div className="row-span-1 bg-custom-gradient rounded-xl flex items-center justify-center m-2 p-4 border-2 border-[#38568e]">
+          <img
+            className="w-2/3"
+            src="src/assets/logo-white.svg"
+            alt="logo-taino"
+          />
         </div>
       </div>
       <div className="col-span-8 w-full bg-[#233f80]/50 h-full justify-baseline items-center flex p-6">
         <div className="swiper carrusel-container h-full  rounded-xl bg-[#091e57]/80">
-          <div className="swiper-wrapper w-full  ">
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+          <div className="swiper-wrapper w-full">
+            <div className="swiper-slide h-full flex items-center justify-center text-white text-xl font-bold ">
+              <div className=" h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className=" h-full flex flex-col justify-center items-center px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
             </div>
             <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py4">
+              <div className="h-full flex flex-col justify-center items-center  px-4 py-4">
                 <img
-                  className="max-h-full w-full"
-                  src="src/assets/galery/galeryExample.png"
+                  className="h-full w-full rounded-xl"
+                  src="src/assets/galery/galeryExample.jpg"
                   alt="galeryexample"
                 />
               </div>
@@ -105,25 +109,29 @@ export default function Carousel() {
         </div>
       </div>
       <div className="text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[#233f80]/50 h-full pr-4 py-4 gap-2 rounded-r-xl">
-        <div className="row-span-1 bg-custom-gradient grid grid-cols-2 grid-rows-1 rounded-xl m-2">
-          <div className="self-center text-center flex flex-row justify-center items-center">
+
+
+        <div className="orbit-container row-span-1 bg-custom-gradient grid grid-cols-2 grid-rows-1 rounded-xl m-2 min-h-[80px]">
+          <div className="orbit-ball"></div>
+          <div className="self-center text-center flex flex-row justify-center items-center z-10">
             <img
               src="src/assets/instagram-white.svg"
               alt="instagram"
               className="w-16 ml-6"
             />
-            <p className=" text-5xl font-thin opacity-70 self-center mb-3 ml-6">
+            <p className="text-5xl font-thin opacity-70 self-center mb-3 ml-6">
               |
             </p>
           </div>
-          <div className="self-center text-center text-6xl justify-center flex mb-2 font-semibold">
-            <p>9K</p>
+
+          <div className="self-center text-center text-6xl justify-center flex mb-2 font-semibold z-10">
+            <p>10K</p>
           </div>
         </div>
-        <div className="row-span-1 bg-custom-gradient rounded-xl flex items-center justify-center m-2">
-          <a href="http://instagram.com/tainobayport/" target="_blank">
-            <p className="text-3xl font-semibold">@TAINOBAYPORT</p>
-          </a>
+
+        
+        <div className="row-span-1 bg-custom-gradient rounded-xl flex items-center justify-center p-4 m-2 border-2 border-[#38568e]">
+          <p className="text-3xl font-semibold">@TAINOBAYPORT</p>
         </div>
       </div>
     </div>
