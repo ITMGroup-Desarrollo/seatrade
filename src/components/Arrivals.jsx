ae8ed9e5747802ff8e1586ad81f97873e07afcfd
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-03-25 16:47:16 -0500

    feat: update Swiper to version 11.2.6 and enhance Arrivals component layout

--- src/components/Arrivals.jsx
+++ src/components/Arrivals.jsx
@@ -1,11 +1,22 @@
 import Pax from "./Pax";
 import Calls from "./Calls";
+import usePort from "../hooks/use-port";
 
 export default function Arrivals() {
+  const name = usePort((state) => state.port.name);
   return (
-    <div className="">
-      <Calls />
-      <Pax />
+    <div className=" h-full px-8 mr-8 py-4">
+      <div className="grid grid-rows-5 h-full rounded-xl bg-[#233f80]/50 border-2 border-[#38568e]">
+        <div className="row-span-1 text-white text-xl font-bold uppercase content-center text-center">
+          Arrivals&nbsp;in <br /> {name}
+        </div>
+        <div className="row-span-2 ">
+          <Calls />
+        </div>
+        <div className="row-span-2 ">
+          <Pax />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,118 +4,67 @@ import "swiper/css/bundle";
 import Card from "./Card";
 import usePort from "../hooks/use-port";
 
-
 export default function Carousel() {
+  const slides = usePort((state) => state.port.slides); // Obtiene el estado
+
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== "undefined" && slides) {
       new Swiper(".carrusel-container", {
         loop: true,
-        slidesPerView: 5,
+        slidesPerView: slides,
         autoplay: {
           delay: 3000,
           disableOnInteraction: false,
         },
       });
     }
-  }, []);
+  }, [slides]);
   const instagram = usePort((state) => state.port.instagram);
   const followers = usePort((state) => state.port.followers);
   const country = usePort((state) => state.port.country);
   const flag = usePort((state) => state.port.flag);
+  const logo = usePort((state) => state.port.logo);
+  const gallery = usePort((state) => state.port.gallery);
+  
+  const galleryClass = usePort((state) => state.port.galleryClass);
+  const socialDisplay = usePort((state) => state.port.socialDisplay);
 
   return (
     <div className="grid grid-rows-1 grid-cols-12 h-full px-8 py-4">
       <div className="text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[#233f80]/50  h-full py-4 pl-4 gap-2 rounded-l-xl">
         <Card className="row-span-1 grid grid-cols-2 grid-rows-1 ">
           <div className="col-span-1 justify-center self-center text-center rounded-xl h-full w-auto">
-            <img
-              src={flag}
-              alt="dominican-flag"
-              className="h-full w-auto"
-            />
+            <img src={flag} alt="dominican-flag" className="h-full w-auto" />
           </div>
-          <div className="col-span-1 justify-center self-center text-left text-lg font-semibold pl-3">
+          <div className="col-span-1 justify-center self-center text-left text-lg font-semibold pl-3 uppercase">
             {country}
           </div>
         </Card>
         <Card className="row-span-1 flex">
-          <img
-            className="w-2/3"
-            src="src/assets/logo-white.svg"
-            alt="logo-taino"
-          />
+          <img className="h-2/3" src={logo} alt="logo-taino" />
         </Card>
       </div>
-      <div className="col-span-8 w-full bg-[#233f80]/50 h-full justify-baseline items-center flex p-6">
+      <div className={`col-span-8 rounded-r-xl w-full bg-[#233f80]/50 h-full justify-baseline items-center flex p-6 ${galleryClass}`}>
         <div className="swiper carrusel-container h-full  rounded-xl bg-[#091e57]/80">
           <div className="swiper-wrapper w-full">
-            <div className="swiper-slide h-full flex items-center justify-center text-white text-xl font-bold ">
-              <div className=" h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
+            {gallery.map((image, index) => (
+              <div
+                key={index}
+                className="swiper-slide h-full flex items-center justify-center text-white text-xl font-bold"
+              >
+                <div className="h-full flex flex-col justify-center items-center px-4 py-4">
+                  <img
+                    className="h-full w-full rounded-xl"
+                    src={image}
+                    alt={`gallery-image-${index}`}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="  h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className=" h-full flex flex-col justify-center items-center px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
-            <div className="swiper-slide h-full   flex items-center justify-center text-white text-xl font-bold ">
-              <div className="h-full flex flex-col justify-center items-center  px-4 py-4">
-                <img
-                  className="h-full w-full rounded-xl"
-                  src="src/assets/galery/galeryExample.jpg"
-                  alt="galeryexample"
-                />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[#233f80]/50 h-full pr-4 py-4 gap-2 rounded-r-xl">
+      <div className={`text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[#233f80]/50 h-full pr-4 py-4 gap-2 rounded-r-xl ${socialDisplay}`}>
         <Card className="row-span-1 grid grid-cols-2 grid-rows-1">
           <div className="self-center text-center flex flex-row justify-center items-center z-10">
             <img
