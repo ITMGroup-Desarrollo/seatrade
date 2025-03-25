6ea4e3bc695528194c3228bae892859094c4b755
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-03-25 17:01:10 -0500

    feat: conditionally hide Arrivals component based on port calls

--- src/components/Arrivals.jsx
+++ src/components/Arrivals.jsx
@@ -4,8 +4,10 @@ import usePort from "../hooks/use-port";
 
 export default function Arrivals() {
   const name = usePort((state) => state.port.name);
+  const calls = usePort((state) => state.port.calls);
+
   return (
-    <div className=" h-full px-8 mr-8 py-4">
+    <div className={`father h-full px-8 mr-8 py-4 ${!calls || calls.length === 0 ? "hidden" : ""}`}>
       <div className="grid grid-rows-5 h-full rounded-xl bg-[#233f80]/50 border-2 border-[#38568e]">
         <div className="row-span-1 text-white text-xl font-bold uppercase content-center text-center">
           Arrivals&nbsp;in <br /> {name}
