2a5fd22d5d06597f21a3f845078991dc7cb42ef2
ItmGroup <hoxte@itmgroup.local>
2025-03-31 17:17:32 -0500

    add hover on state map

--- src/hooks/use-port.js
+++ src/hooks/use-port.js
@@ -5,9 +5,16 @@ const DEFAULT_PORT = "cabo-rojo";
 
 const usePort = create((set, get) => ({
   port: dataPorts[DEFAULT_PORT],
-  selectPort: (portId) => {
+  hoveredPort: null,
+  setPort: (portId) => {
     const selectedPort = dataPorts[portId];
     set({ port: selectedPort });
+  },
+  setHoveredPort: (portId) => {
+    const selectedPort2 = dataPorts[portId]?.name;
+    console.log(selectedPort2);
+
+    set({ hoveredPort: selectedPort2 });
   },
   // removePort: () => set({ port: 0 }),
   // updatePort: (newPort) => set({ port: newPort }),
