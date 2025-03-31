4b7ed8fb70b77905078cb9efef6ffa9e39dbc386
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-03-31 15:46:44 -0500

    feat: add fade-in animation utility and refactor Arrivals and Carousel components for improved dynamic behavior

--- src/components/Arrivals.jsx
+++ src/components/Arrivals.jsx
@@ -10,18 +10,18 @@ import { useState, useEffect } from "react";
 export default function Arrivals() {
   const name = usePort((state) => state.port.name);
   const calls = usePort((state) => state.port.calls);
-  const [ruedaKey, setRuedaKey] = useState(0);
+  const [eventKey, setEventKey] = useState(0);
 
   useEffect(() => {
-    const resetRueda = () => {
-      setRuedaKey((prevKey) => prevKey + 1);
+    const resetEvent = () => {
+      setEventKey((prevKey) => prevKey + 1);
     };
 
     const icons = document.querySelectorAll(".icon-mark");
-    icons.forEach((icon) => icon.addEventListener("click", resetRueda));
+    icons.forEach((icon) => icon.addEventListener("click", resetEvent));
 
     return () => {
-      icons.forEach((icon) => icon.removeEventListener("click", resetRueda));
+      icons.forEach((icon) => icon.removeEventListener("click", resetEvent));
     };
   }, []);
 
@@ -32,16 +32,16 @@ export default function Arrivals() {
       }`}
     >
       <div className="grid grid-rows-5 h-full rounded-xl bg-[var(--color-secondary)]/50 border-2 border-[var(--color-fifth-border)] gap-2">
-        <div className="row-span-1 text-white text-xl font-bold uppercase content-center text-center">
+        <div key={eventKey} className="row-span-1 text-white text-xl font-bold uppercase content-center text-center animate-fadeIn">
           Arrivals&nbsp;in <br /> {name}
         </div>
         <div className="row-span-2 grid grid-rows-3 ">
           <div className="relative row-span-2 flex justify-center items-center text-center">
             <div>
-              <img src={CallsIcon.src} alt="calls" className="w-12 z-10 icon-mark" />
+              <img src={CallsIcon.src} alt="calls" className="w-12 z-10 " />
               <p className="textArrivals font-medium">CALLS</p>
             </div>
-             <RuedaCalls key={ruedaKey} className="calls-container"/>
+             <RuedaCalls key={eventKey} className="calls-container animate-fadeIn"/>
           </div>
 
           <div className="row-span-1 text-center content-center text-white font-bold text-5xl ">
@@ -49,12 +49,12 @@ export default function Arrivals() {
           </div>
         </div>
         <div className="row-span-2 grid grid-rows-3 ">
-          <div className="pax-Container relative row-span-2 flex justify-center items-center text-center">
+          <div className="pax-Container relative row-span-2 flex justify-center items-center text-center ">
             <div>
-              <img src={PaxIcon.src} alt="Pax" className="w-12 z-10 icon-mark" />
+              <img src={PaxIcon.src} alt="Pax" className="w-12 z-10 " />
               <p className="textArrivals font-medium">PAX</p>
             </div>
-            <RuedaPax key={ruedaKey} className="pax-container" />
+            <RuedaPax key={eventKey} className="pax-container animate-fadeIn" />
           </div>
           <div className="row-span-1 text-center content-center text-white font-semibold text-3xl">
             <Pax />
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 import Card from "./Card";
@@ -10,6 +10,7 @@ export default function Carousel() {
   const slides = usePort((state) => state.port.slides);
   const port = usePort((state) => state.port);
   const swiperRef = useRef(null);
+  const [eventKey, setEventKey] = useState(0);
 
   const initializeSwiper = () => {
     if (swiperRef.current) {
@@ -49,39 +50,46 @@ export default function Carousel() {
         initializeSwiper(); 
       }
     };
-
+    const resetEvent = () => {
+      setEventKey((prevKey) => prevKey + 1);
+    };
     const icons = document.querySelectorAll(".icon-mark");
     icons.forEach((icon) => icon.addEventListener("click", resetSwiper));
+    icons.forEach((icon) => icon.addEventListener("click", resetEvent));
 
     return () => {
       icons.forEach((icon) => icon.removeEventListener("click", resetSwiper));
+      icons.forEach((icon) => icon.removeEventListener("click", resetEvent));
     };
+
+    
   }, []);
 
   return (
     <div className="grid grid-rows-1 grid-cols-12 h-full px-8 py-4">
       <div className="text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[var(--color-secondary)]/50  h-full py-4 pl-4 gap-2 rounded-l-xl">
-        <Card className="row-span-1 grid grid-cols-2 grid-rows-1 ">
-          <div className="col-span-1 justify-center self-center text-center rounded-xl h-full w-auto">
+        <Card  className="row-span-1 grid grid-cols-2 grid-rows-1 ">
+          <div  className="col-span-1 justify-center self-center text-center rounded-xl h-full w-auto ">
             <img
               src={port.flag}
               alt="dominican-flag"
-              className="h-full w-auto"
+              key={eventKey}
+              className="h-full w-auto animate-fadeIn"
             />
           </div>
-          <div className="col-span-1 justify-center self-center text-left text-lg font-semibold pl-3 uppercase">
+          <div key={eventKey} className="col-span-1 justify-center self-center text-left text-lg font-semibold pl-3 uppercase animate-fadeIn">
             {port.country}
           </div>
         </Card>
         <Card className="row-span-1 flex">
-          <img className="h-2/3" src={port.logo} alt="logo-taino" />
+          <img key={eventKey} className="h-2/3 animate-fadeIn" src={port.logo} alt="logo-taino" />
         </Card>
       </div>
       <div
         className={`col-span-8 w-full bg-[var(--color-secondary)]/50 h-full justify-baseline items-center flex p-6 ${port.galleryClass}`}
       >
         <div className="swiper carrusel-container h-full rounded-xl bg-[var(--color-tertiary)]/80">
-          <div className="swiper-wrapper w-full">
+          <div  className="swiper-wrapper w-full">
             {port.gallery.map((image, index) => (
               <div
                 key={index}
@@ -89,8 +97,9 @@ export default function Carousel() {
               >
                 <div className="h-full flex flex-col justify-center items-center px-4 py-4">
                   <img
-                    className="h-full w-full rounded-xl"
+                    className="h-full w-full rounded-xl animate-fadeIn"
                     src={image}
+                    key={eventKey}
                     alt={`gallery-image-${index}`}
                   />
                 </div>
@@ -103,7 +112,7 @@ export default function Carousel() {
         className={`text-white col-span-2 grid grid-cols-1 grid-rows-2 bg-[var(--color-secondary)]/50 h-full pr-4 py-4 gap-2 rounded-r-xl ${port.socialDisplay}`}
       >
         <Card className="row-span-1 grid grid-cols-2 grid-rows-1">
-          <div className="self-center text-center flex flex-row justify-center items-center z-10">
+          <div  className="self-center text-center flex flex-row justify-center items-center z-10">
             <img
               src={logoInstagram.src}
               alt="instagram"
@@ -114,15 +123,15 @@ export default function Carousel() {
             </p>
           </div>
 
-          <div className="self-center text-center text-6xl justify-center flex mb-2 font-semibold z-10">
-            <p>{port.followers}</p>
+          <div  className="self-center text-center text-6xl justify-center flex mb-2 font-semibold z-10 ">
+            <p key={eventKey} className="animate-fadeIn">{port.followers}</p>
           </div>
         </Card>
 
         <Card className="row-span-1 flex flex-col">
-          <p className="text-3xl font-semibold">{port.instagram}</p>
+          <p key={eventKey} className="text-3xl font-semibold animate-fadeIn">{port.instagram}</p>
           <div>
-            <img src={port.qr} width={80} alt="" />
+            <img key={eventKey} className="animate-fadeIn" src={port.qr} width={80} alt="" />
           </div>
         </Card>
       </div>
